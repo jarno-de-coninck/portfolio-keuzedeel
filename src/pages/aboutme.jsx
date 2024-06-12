c6cb0939bec871b70fff8f831423bedc6efa2479
Jarno8669 <105166348+Jarno8669@users.noreply.github.com>
2024-06-12 21:49:38 +0200

    Update
    yes yes

--- src/pages/aboutme.jsx
+++ src/pages/aboutme.jsx
@@ -6,7 +6,7 @@ function AboutMe() {
       <h1 className='fw-bold'>Over mij</h1>
       <div className="row justify-content-between align-items-center d-flex h-75">
         <div className="col-md-3 d-flex flex-column text-center h-50">
-          <img className='mx-auto' src="./images/myself.png" alt="" width={120}/>
+          <img className='mx-auto mb-4' src="./images/myself.png" alt="" width={120}/>
           <h3 className='mainColor mt-auto'>Project Roi</h3>
           <p>Learn more about my hobbies and teamroles.</p>
           <div className="d-flex flex-column gap-2">
@@ -15,13 +15,13 @@ function AboutMe() {
           </div>
         </div>
         <div className="col-md-3 d-flex flex-column text-center h-50">
-          <img className='mx-auto' src="./images/education.png" alt="" width={120}/>
+          <img className='mx-auto mb-4' src="./images/education.png" alt="" width={120}/>
           <h3 className='mainColor mt-auto'>Project Roi</h3>
           <p>Find out why I chose this career path and how it is going so far.</p>
           <button className='btn px-4 fw-bold text-white w-50 mx-auto'>Learn more</button>
         </div>
         <div className="col-md-3 d-flex flex-column text-center h-50">
-          <img className='mx-auto' src="./images/skills.png" alt="" width={120}/>
+          <img className='mx-auto mb-4' src="./images/skills.png" alt="" width={120}/>
           <h3 className='mainColor mt-auto'>Project Roi</h3>
           <p>Find out what kinds of skill I have.</p>
           <button className='btn px-4 fw-bold text-white w-50 mx-auto'>Learn more</button>
