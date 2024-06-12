946223d14c2ddf5d39b6b5e51acefe3c655403fa
Jarno8669 <105166348+Jarno8669@users.noreply.github.com>
2024-06-12 22:30:54 +0200

    Added scrolling frame

--- src/pages/projects.jsx
+++ src/pages/projects.jsx
@@ -1,54 +1,69 @@
-import { useState } from 'react'
+import React from 'react';
 
 function Projects() {
-  return (  
-    <div className="h-75 my-auto w-100">
-      <h1 className='fw-bold'>Projecten</h1>
-      <div className="scrollingFrame row w-100">
-        <div className="col-md-3 projectElement d-flex flex-column justify-content-between align-items-center p-2 rounded-2">
-          <img src="https://storage.pubble.nl/65142601/content/2022/10/5fb76747-4b09-43c3-82a2-2973231ee4e5_thumb1920.jpg" alt="" className='w-100 rounded-2' />
-          <h3 className='mainColor mt-2'>Kaasboederij</h3>
-          <p className='m-0'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam  facilisis ultricies tellus.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam  facilisis ultricies tellus.</p>
-        </div>
-        <div className="col-md-3 projectElement d-flex flex-column justify-content-between align-items-center p-2 rounded-2">
-          <img src="https://storage.pubble.nl/65142601/content/2022/10/5fb76747-4b09-43c3-82a2-2973231ee4e5_thumb1920.jpg" alt="" className='w-100 rounded-2' />
-          <h3 className='mainColor mt-2'>Kaasboederij</h3>
-          <p className='m-0'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam  facilisis ultricies tellus.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam  facilisis ultricies tellus.</p>
-        </div>
-        <div className="col-md-3 projectElement d-flex flex-column justify-content-between align-items-center p-2 rounded-2">
-          <img src="https://storage.pubble.nl/65142601/content/2022/10/5fb76747-4b09-43c3-82a2-2973231ee4e5_thumb1920.jpg" alt="" className='w-100 rounded-2' />
-          <h3 className='mainColor mt-2'>Kaasboederij</h3>
-          <p className='m-0'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam  facilisis ultricies tellus.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam  facilisis ultricies tellus.</p>
-        </div>
-        <div className="col-md-3 projectElement d-flex flex-column justify-content-between align-items-center p-2 rounded-2">
-          <img src="https://storage.pubble.nl/65142601/content/2022/10/5fb76747-4b09-43c3-82a2-2973231ee4e5_thumb1920.jpg" alt="" className='w-100 rounded-2' />
-          <h3 className='mainColor mt-2'>Kaasboederij</h3>
-          <p className='m-0'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam  facilisis ultricies tellus.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam  facilisis ultricies tellus.</p>
-        </div>
-        <div className="col-md-3 projectElement d-flex flex-column justify-content-between align-items-center p-2 rounded-2">
-          <img src="https://storage.pubble.nl/65142601/content/2022/10/5fb76747-4b09-43c3-82a2-2973231ee4e5_thumb1920.jpg" alt="" className='w-100 rounded-2' />
-          <h3 className='mainColor mt-2'>Kaasboederij</h3>
-          <p className='m-0'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam  facilisis ultricies tellus.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam  facilisis ultricies tellus.</p>
+  return (
+    <div className="container-fluid">
+      <div className="h-100 my-auto w-100">
+        <h1 className="fw-bold">Projecten</h1>
+        <div className="scrollingFrame d-flex overflow-x-scroll">
+
+          <div className="projectElement mx-2">
+            <img src="https://storage.pubble.nl/65142601/content/2022/10/5fb76747-4b09-43c3-82a2-2973231ee4e5_thumb1920.jpg" alt="Kaasboederij" className="img-fluid rounded-2" />
+            <h3 className="mainColor mt-2">Kaasboederij</h3>
+            <p className="m-0">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam facilisis ultricies tellus. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam facilisis ultricies tellus.</p>
+          </div>
+          
+          <div className="projectElement mx-2">
+            <img src="https://storage.pubble.nl/65142601/content/2022/10/5fb76747-4b09-43c3-82a2-2973231ee4e5_thumb1920.jpg" alt="Kaasboederij" className="img-fluid rounded-2" />
+            <h3 className="mainColor mt-2">Kaasboederij</h3>
+            <p className="m-0">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam facilisis ultricies tellus. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam facilisis ultricies tellus.</p>
+          </div>
+
+          <div className="projectElement mx-2">
+            <img src="https://storage.pubble.nl/65142601/content/2022/10/5fb76747-4b09-43c3-82a2-2973231ee4e5_thumb1920.jpg" alt="Kaasboederij" className="img-fluid rounded-2" />
+            <h3 className="mainColor mt-2">Kaasboederij</h3>
+            <p className="m-0">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam facilisis ultricies tellus. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam facilisis ultricies tellus.</p>
+          </div>
+
+          <div className="projectElement mx-2">
+            <img src="https://storage.pubble.nl/65142601/content/2022/10/5fb76747-4b09-43c3-82a2-2973231ee4e5_thumb1920.jpg" alt="Kaasboederij" className="img-fluid rounded-2" />
+            <h3 className="mainColor mt-2">Kaasboederij</h3>
+            <p className="m-0">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam facilisis ultricies tellus. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam facilisis ultricies tellus.</p>
+          </div>
+
+          <div className="projectElement mx-2">
+            <img src="https://storage.pubble.nl/65142601/content/2022/10/5fb76747-4b09-43c3-82a2-2973231ee4e5_thumb1920.jpg" alt="Kaasboederij" className="img-fluid rounded-2" />
+            <h3 className="mainColor mt-2">Kaasboederij</h3>
+            <p className="m-0">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam facilisis ultricies tellus. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam facilisis ultricies tellus.</p>
+          </div>
+
+          <div className="projectElement mx-2">
+            <img src="https://storage.pubble.nl/65142601/content/2022/10/5fb76747-4b09-43c3-82a2-2973231ee4e5_thumb1920.jpg" alt="Kaasboederij" className="img-fluid rounded-2" />
+            <h3 className="mainColor mt-2">Kaasboederij</h3>
+            <p className="m-0">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam facilisis ultricies tellus. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam facilisis ultricies tellus.</p>
+          </div>
+
+          <div className="projectElement mx-2">
+            <img src="https://storage.pubble.nl/65142601/content/2022/10/5fb76747-4b09-43c3-82a2-2973231ee4e5_thumb1920.jpg" alt="Kaasboederij" className="img-fluid rounded-2" />
+            <h3 className="mainColor mt-2">Kaasboederij</h3>
+            <p className="m-0">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam facilisis ultricies tellus. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam facilisis ultricies tellus.</p>
+          </div>
+
+          <div className="projectElement mx-2">
+            <img src="https://storage.pubble.nl/65142601/content/2022/10/5fb76747-4b09-43c3-82a2-2973231ee4e5_thumb1920.jpg" alt="Kaasboederij" className="img-fluid rounded-2" />
+            <h3 className="mainColor mt-2">Kaasboederij</h3>
+            <p className="m-0">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam facilisis ultricies tellus. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam facilisis ultricies tellus.</p>
+          </div>
+
+          <div className="projectElement mx-2">
+            <img src="https://storage.pubble.nl/65142601/content/2022/10/5fb76747-4b09-43c3-82a2-2973231ee4e5_thumb1920.jpg" alt="Kaasboederij" className="img-fluid rounded-2" />
+            <h3 className="mainColor mt-2">Kaasboederij</h3>
+            <p className="m-0">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam facilisis ultricies tellus. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam facilisis ultricies tellus.</p>
+          </div>
         </div>
       </div>
-
-      <div class="row row-horizon">
-        <div class="col-md-6">
-          <h1>Test</h1>
-        </div>
-        <div class="col-md-6">
-          <h1>Test</h1>
-        </div>
-        <div class="col-md-6">
-          <h1>Test</h1>
-        </div>
-        <div class="col-md-6">
-          <h1>Test</h1>
-        </div>
-      </div>
-
     </div>
-  )
+  );
 }
 
-export default Projects
+export default Projects;
